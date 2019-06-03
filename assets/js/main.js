d41aeb2d97f5b83b0170370ffe743d9f077eca85
AnthonyMoreau <a82moreau@gmail.com>
2019-06-03 16:17:32 +0200

    commit anthony

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 // ADDITION//////////
-
 let $divFirstAddition = $(".addition .first")[0]
 let $divSecondAddition = $(".addition .second")[0]
 let $resultAddition = $(".addition .result")[0]
@@ -10,7 +9,6 @@ let firstAddition = undefined
 let secondAddition = undefined
 
 // SOUSTRACTION//////////
-
 let $divFirstSoustraction = $(".soustraction .first")[0]
 let $divSecondSoustraction = $(".soustraction .second")[0]
 let $resultSoustraction = $(".soustraction .result")[0]
@@ -19,7 +17,6 @@ let firstSoustraction = undefined
 let secondSoustraction = undefined
 
 // DIVISION/////////
-
 let $divFirstDivision = $(".division .first")[0]
 let $divSecondDivision = $(".division .second")[0]
 let $resultDivision = $(".division .result")[0]
@@ -28,7 +25,6 @@ let firstDivision = undefined
 let secondDivision = undefined
 
 // MULTIPLICATION////////
-
 let $divFirstMultiplication = $(".multiplication .first")[0]
 let $divSecondMultiplication = $(".multiplication .second")[0]
 let $resultMultiplication = $(".multiplication .result")[0]
@@ -47,33 +43,57 @@ let calcul = function(a, b, operateur){
         result = a - b
     }    
     if(operateur === "/"){
+        if( a === 0 || b === 0 ){
+            result = "Infinity"
+        }
         result = a / b
     }    
     if(operateur === "*"){
         result = a * b
-    }    
+    } 
+
     return result
 }    
 
+// retourne le resulta
 let interval = setInterval(function(){
 
-     first = Number.parseInt($divFirstAddition.value)
-     second = Number.parseInt($divSecondAddition.value)
+    firstAddition = Number.parseInt($divFirstAddition.value)
+    secondAddition = Number.parseInt($divSecondAddition.value)
 
-     if(isNaN(first) === true){
-
-        first = 0 
-     }   
-     if(isNaN(second) === true){
-
-        second = 0 
-     }   
+    firstSoustraction = Number.parseInt($divFirstSoustraction.value)
+    secondSoustraction = Number.parseInt($divSecondSoustraction.value)
     
+
+     if(isNaN(firstAddition) === true){
+
+        firstAddition = 0 
+     }   
+     if(isNaN(secondAddition) === true){
+
+        secondAddition = 0 
+     }
+     if(isNaN(firstSoustraction) === true){
+
+        firstSoustraction = 0 
+     }   
+     if(isNaN(secondSoustraction) === true){
+
+        secondSoustraction = 0 
+     }  
+    
+
+
+
+
+
+
     $results.click(function(){
     
         $resultAddition.value = calcul(firstAddition, secondAddition, "+")
+        $resultSoustraction.value = calcul(firstSoustraction, secondSoustraction, "-")
     })    
 
 }, 1000/24)    
 
-
+debugger
